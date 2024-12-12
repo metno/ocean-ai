3457d8dfefb71e3c2bbb95589127ec987c9def65
Ina Kullmann <ina.k.kullmann@met.no>
2024-12-12 14:35:59 +0000

    training using qsub working

--- ppi_train.sh
+++ ppi_train.sh
@@ -6,19 +6,14 @@
 #$ -l h_rss=20G
 #$ -l h_rt=01:00:00
 #$ -q gpu-r8.q
-#$ -l h=gpu-04.ppi.met.no
-#$ -o /lustre/storeB/project/fou/hi/foccus/mateuszm/OceanAI/logs/OUT_$JOB_NAME.$JOB_ID
-#$ -e /lustre/storeB/project/fou/hi/foccus/mateuszm/OceanAI/logs/ERR_$JOB_NAME.$JOB_ID
+#$ -l h=sm-nx10077659-bc-compute.int.met.no
+#$ -o /lustre/storeB/project/fou/hi/foccus/outputs/OUT_$JOB_NAME.$JOB_ID
+#$ -e /lustre/storeB/project/fou/hi/foccus/outputs/ERR_$JOB_NAME.$JOB_ID
 
-DIR=/lustre/storeB/project/fou/hi/foccus/mateuszm/OceanAI/
+DIR=/lustre/storeB/project/fou/hi/foccus
+cd $DIR/experiments/temp-jan
 export HYDRA_FULL_ERROR=1
 export ANEMOI_BASE_SEED=1
 conda deactivate
-source /lustre/storeB/project/fou/hi/foccus/python-envs/new-anemoi-env/bin/activate
-anemoi-training train --config-dir=//lustre/storeB/project/fou/hi/foccus/mateuszm/OceanAI/training/ --config-name=master.yaml
-
-
-#conda deactivate
-#source /lustre/storeB/project/fou/hi/foccus/mateuszm/testing_envs/anemoi-env/bin/activate
-#nemoi-training train --config-name=//lustre/storeB/project/fou/hi/foccus/mateuszm/aifs-mono-ocean/aifs/config/mateusz_metno_ocean.yaml
-#python $PYTHON_SCRIPT
+source $DIR/python-envs/new-anemoi-env/bin/activate
+anemoi-training train --config-dir=$DIR/ina/ocean-ai/training/ --config-name=master.yaml
