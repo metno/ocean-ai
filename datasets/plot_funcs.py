6189b1efe3cbc60c5f0dcd86713328b7c7af9e83
Ina Kullmann <ina.k.kullmann@met.no>
2024-12-17 12:00:16 +0000

    pcolormesh animation working. using interpolation with resolution=0.01

--- datasets/plot_funcs.py
+++ datasets/plot_funcs.py
@@ -1,10 +1,75 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+#import anemoi.datasets as ad
+
+def simple_mesh_plot(lon_grid, lat_grid, var_grid, time,var_name='temperature'):
+    # TODO: add options for **kwargs
+    kwargs = {}
+    kwargs['shading'] = 'auto' #kwargs['vmin'] = vmin; kwargs['vmax'] = vmax #se også på norm
+    kwargs['cmap'] = 'viridis'
+
+    fig, ax = plt.subplots(figsize=(8, 6),squeeze=False, subplot_kw={'projection': ccrs.PlateCarree()})
+    ax1=ax[0,0]
+    ax1.add_feature(cfeature.COASTLINE)
+    ax1.add_feature(cfeature.BORDERS, linestyle=':')
+    ax1.add_feature(cfeature.LAND, edgecolor='black')
+    
+    heatmap = ax1.pcolormesh(lon_grid, lat_grid, var_grid, **kwargs)
+    # TODO improve this cbar and fig size etc
+    #cbar = fig.colorbar(heatmap, ax=ax1, orientation='vertical')
+    #cbar.set_label(var_name)
+    ax1.set_title(f'Time step: {time}')
+
+    return fig, ax1, heatmap#, cbar
+
+def update_anim(frame,ax,heatmap,var,time):
+    heatmap.set_array(var[frame].ravel())
+    ax.set_title(f'Time step: {time[frame]}')
+
+def subset_dataset(zda, lon_min, lon_max, lat_min, lat_max, ntime=12, var_name='temperature_1'):
+    """
+    Subsets the dataset to the specified longitude and latitude range,
+    and returns a new dataset containing only the longitude, latitude,
+    and temperature_1 variables.
+    
+    Parameters:
+    ds (anemoi.datasets): The input dataset.
+    lon_min (float): Minimum longitude.
+    lon_max (float): Maximum longitude.
+    lat_min (float): Minimum latitude.
+    lat_max (float): Maximum latitude.
+    var_name (string): Name of the variable to select a subset of. 
+    
+    Returns:
+    ...
+    """
+    # Extract longitude, latitude, and var
+    longitude = zda.longitudes
+    latitude = zda.latitudes
+
+    # Create a mask for the bounding box
+    lon_mask = (longitude >= lon_min) & (longitude <= lon_max)
+    lat_mask = (latitude >= lat_min) & (latitude <= lat_max)
+    
+    # Combine masks
+    combined_mask = lon_mask & lat_mask
+    
+    # Apply the mask to filter the data
+    filtered_longitude = longitude[combined_mask]
+    filtered_latitude = latitude[combined_mask]
+    
+    indx_var = zda.name_to_index[var_name]
+    var = zda[:ntime,indx_var,0,:]
+    filtered_var = var[:, combined_mask]
+
+    return filtered_var,filtered_latitude,filtered_longitude
 
 def plot_dataset(zda, var_name, indx_time=0): #, cmin=10, cmax=-2):
     # The data are defined in lat/lon coordinate system, so PlateCarree()
     # is the appropriate choice:
+    """zda is a zarr file opened with anemoi datasets..."""
     data_crs = ccrs.PlateCarree()
 
     proj = ccrs.LambertConformal(
@@ -16,8 +81,6 @@ def plot_dataset(zda, var_name, indx_time=0): #, cmin=10, cmax=-2):
     fig = plt.figure(figsize=(20,20))
     ax = plt.axes(projection=proj)
     ax.coastlines()
-    print("hello2")
-
 
     # Get the index of the variable and the variable itself
     indx_var = zda.name_to_index[var_name]
