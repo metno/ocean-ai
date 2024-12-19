23f6669692a9fcef77c74516ef0ccbfa603c3043
Ina Kullmann <inakbk@protonmail.com>
2024-12-19 13:45:46 +0100

    Generalise ppi-sub.sh

--- datasets/ppi-sub.sh
+++ datasets/ppi-sub.sh
@@ -2,11 +2,10 @@
 #$ -l h_rt=48:00:00
 #$ -q research-r8.q
 #$ -l h_rss=20G,mem_free=20G,h_data=20G
-#$ -o /lustre/storeB/project/fou/hi/foccus/mateuszm/OceanAI/datasets/logs/OUT_$JOB_NAME.$JOB_ID
-#$ -e /lustre/storeB/project/fou/hi/foccus/mateuszm/OceanAI/datasets/logs/ERR_$JOB_NAME.$JOB_ID
+#$ -o /lustre/storeB/project/fou/hi/foccus/outputs/OUT_$JOB_NAME.$JOB_ID
+#$ -e /lustre/storeB/project/fou/hi/foccus/outputs/ERR_$JOB_NAME.$JOB_ID
 #$ -N h
 #$ -t 1-366
-
 
 YEAR=2024
 FOCCUS_DIR=/lustre/storeB/project/fou/hi/foccus/
