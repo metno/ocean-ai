fecf2993b98731160d81eeabb4e2af141bb6d213
Ina Kullmann <ina.k.kullmann@met.no>
2025-01-24 08:49:20 +0200

    working training configs and sub. scripts. Using model paralell

--- lumi_train.sh
+++ lumi_train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
-#SBATCH --job-name=ocean-ai   # Job name
+#SBATCH --job-name=ocean-ai-allvars   # Job name
 #SBATCH --output=outputs/output.o%j # Name of stdout output file
 #SBATCH --error=outputs/error.e%j  # Name of stderr error file
-#SBATCH --partition=dev-g  # partition name
+#SBATCH --partition=standard-g  # partition name
 #SBATCH --nodes=1               # Total number of nodes 
 #SBATCH --ntasks-per-node=8     # 8 MPI ranks per node, 16 total (2x8)
 #SBATCH --gpus-per-node=8       # Allocate one gpu per MPI rank
-#SBATCH --time=0:10:00       # Run time (d-hh:mm:ss)
+#SBATCH --time=10:00:00       # Run time (d-hh:mm:ss)
 #SBATCH --account=project_465001629 # Project for billing
 
 #Change this
