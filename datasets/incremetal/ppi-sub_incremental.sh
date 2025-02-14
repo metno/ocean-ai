76ce5d3facbb841e46e20fb9985134e4003e7745
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2025-02-14 13:52:55 +0000

    creating mask zarr for 2024

--- datasets/incremetal/ppi-sub_incremental.sh
+++ datasets/incremetal/ppi-sub_incremental.sh
@@ -1,17 +1,17 @@
 #$ -S /bin/bash
-#$ -l h_rt=12:00:00
+#$ -l h_rt=2:00:00
 #$ -q research-r8.q
 #$ -l h_rss=20G,mem_free=20G,h_data=20G
 #$ -o /lustre/storeB/project/fou/hi/foccus/outputs/OUT_$JOB_NAME.$JOB_ID
 #$ -e /lustre/storeB/project/fou/hi/foccus/outputs/ERR_$JOB_NAME.$JOB_ID
-#$ -N h
+#$ -N mask
 #$ -t 1-366
 
 YEAR=2024
 FOCCUS_DIR=/lustre/storeB/project/fou/hi/foccus/
 OUTDIR=$FOCCUS_DIR/datasets/
 #YAMLFILE=$FOCCUS_DIR/mateuszm/OceanAI/datasets/mask_zarr.yaml
-ZARRFILE=$OUTDIR/constant_h_norkyst_${YEAR}.zarr
+ZARRFILE=$OUTDIR/constant_mask_norkyst_${YEAR}.zarr
 
 conda deactivate
 source $FOCCUS_DIR/python-envs/anemoi-env-14-2-2025/bin/activate
