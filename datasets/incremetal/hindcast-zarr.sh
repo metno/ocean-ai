a2239265303612847c103ca2b5c3cf97431c3b97
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2025-04-15 07:35:47 +0000

    files used to create hindcast 2023

--- datasets/incremetal/hindcast-zarr.sh
+++ datasets/incremetal/hindcast-zarr.sh
@@ -1,11 +1,11 @@
 #$ -S /bin/bash
-#$ -l h_rt=2:00:00
+#$ -l h_rt=0:30:00
 #$ -q bigmem-r8.q
 #$ -l h_rss=81G,mem_free=81G,h_data=81G
 #$ -o /lustre/storeB/project/fou/hi/foccus/mateuszm/outputs/
 #$ -e /lustre/storeB/project/fou/hi/foccus/mateuszm/outputs/
 #$ -N hindcast2023
-#$ -t 1-2
+#$ -t 2-366
 
 YEAR=2023
 FOCCUS_DIR=/lustre/storeB/project/fou/hi/foccus/
@@ -16,5 +16,5 @@ ZARRFILE=$OUTDIR/norkystv3_hindcast_${YEAR}.zarr
 conda deactivate
 source $FOCCUS_DIR/python-envs/anemoi-env-8-4-25/bin/activate
 #anemoi-datasets init $YAMLFILE $ZARRFILE --overwrite 
-anemoi-datasets load $ZARRFILE --part $SGE_TASK_ID/366
+anemoi-datasets load $ZARRFILE --part $SGE_TASK_ID/366 #8784
 #anemoi-datasets finalize $ZARRFILE
