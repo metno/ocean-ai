352f2c9d58f6fc095a0abf6d19a6086c4bff6615
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2025-04-22 12:52:03 +0000

    hindcast 2022 files

--- datasets/create_dataset_yaml_hindcast.py
+++ datasets/create_dataset_yaml_hindcast.py
@@ -82,6 +82,6 @@ def create_dataset_yaml_file(start = datetime.datetime(2024,1,1,0), end = dateti
         yaml.dump(input_dict, f, sort_keys=False)
 
 if __name__ == '__main__':
-    params_list = ['ln_AKs', 'temperature', 'salinity', 'u_eastward', 'v_northward', 'ubar_eastward', 'vbar_northward', 'zeta', 'Uwind_eastward', 'Vwind_northward']
+    params_list = ['temperature', 'salinity', 'u_eastward', 'v_northward', 'ubar_eastward', 'vbar_northward', 'zeta', 'Uwind_eastward', 'Vwind_northward']
     nan_list = params_list
-    create_dataset_yaml_file(start = datetime.datetime(2023,1,1,0), end=datetime.datetime(2023,12,31,23), params_list=params_list, nan_list=nan_list, outfile='yaml_files/norkystv3-hindcast-2023.yaml')
+    create_dataset_yaml_file(start = datetime.datetime(2022,1,1,0), end=datetime.datetime(2022,12,31,23), params_list=params_list, nan_list=nan_list, outfile='yaml_files/norkystv3-hindcast-2022.yaml')
