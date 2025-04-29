480fb97203dc7d7e4fd4ee70477886133561a3a7
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2025-04-29 07:23:56 +0000

    2019

--- datasets/incremetal/hindcast-zarr.sh
+++ datasets/incremetal/hindcast-zarr.sh
@@ -4,10 +4,10 @@
 #$ -l h_rss=30G,mem_free=30G,h_data=30G
 #$ -o /lustre/storeB/project/fou/hi/foccus/mateuszm/outputs/
 #$ -e /lustre/storeB/project/fou/hi/foccus/mateuszm/outputs/
-#$ -N surfacehindcast2020
+#$ -N surfacehindcast2019
 #$ -t 1-366
 
-YEAR=2020
+YEAR=2019
 FOCCUS_DIR=/lustre/storeB/project/fou/hi/foccus/
 OUTDIR=$FOCCUS_DIR/datasets/
 ZARRFILE=$OUTDIR/norkystv3_hindcast_${YEAR}_surface.zarr
