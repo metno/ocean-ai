c4c23a873c89fef52b36a30759ee132bbecd03c6
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2025-05-13 08:51:12 +0000

    submitting forcing

---
+++ datasets/incremetal/forcing-zarr.sh
@@ -0,0 +1,19 @@
+#$ -S /bin/bash
+#$ -l h_rt=0:20:00
+#$ -q bigmem-r8.q
+#$ -l h_rss=81G,mem_free=81G,h_data=81G
+#$ -o /lustre/storeB/project/fou/hi/foccus/mateuszm/outputs/
+#$ -e /lustre/storeB/project/fou/hi/foccus/mateuszm/outputs/
+#$ -N forcing
+#$ -t 1-41
+
+DATE=2023010100-2023050100
+FOCCUS_DIR=/lustre/storeB/project/fou/hi/foccus/
+OUTDIR=$FOCCUS_DIR/datasets/
+ZARRFILE=$OUTDIR/forcing_norkystv3_hindcast_${DATE}.zarr
+
+conda deactivate
+source $FOCCUS_DIR/python-envs/anemoi-env-16-4-25/bin/activate
+#anemoi-datasets init $YAMLFILE $ZARRFILE --overwrite 
+anemoi-datasets load $ZARRFILE --part $SGE_TASK_ID/83 #8784
+#anemoi-datasets finalize $ZARRFILE
