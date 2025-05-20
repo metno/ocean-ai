56441b723e9011be73e0a1e20c5d0c3924f0ca71
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2025-05-20 08:59:04 +0000

    quick and dirty script to find missing dates in forcing files

---
+++ forcing/identify_missing_date.py
@@ -0,0 +1,28 @@
+"""
+script for finding missing dates in forcing data
+Author: Mateusz Matuszak
+"""
+
+import xarray as xr
+import pandas as pd
+
+def missing_date(file):
+    ds = xr.open_dataset(file)
+    file_times = ds.time.values
+    date_range = pd.date_range(start=file_times[0], end=file_times[-1], freq='3H')
+    
+    missing_dates = []
+    for time in date_range:
+        if time not in file_times:
+            missing_dates.append(time)
+    
+    for time in missing_dates:
+        print(time)
+        
+    return missing_dates
+    
+
+if __name__ == '__main__':
+    import sys
+    file = sys.argv[1]
+    missing_date(file)
