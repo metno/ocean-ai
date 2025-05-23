6cea21af137580768877d8c96d765b38070f624b
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2025-05-23 12:43:20 +0000

    change in submit

--- datasets/incremetal/forcing-zarr.sh
+++ datasets/incremetal/forcing-zarr.sh
@@ -1,13 +1,13 @@
 #$ -S /bin/bash
-#$ -l h_rt=0:20:00
+#$ -l h_rt=1:00:00
 #$ -q bigmem-r8.q
 #$ -l h_rss=81G,mem_free=81G,h_data=81G
 #$ -o /lustre/storeB/project/fou/hi/foccus/mateuszm/outputs/forcing/
 #$ -e /lustre/storeB/project/fou/hi/foccus/mateuszm/outputs/forcing/
 #$ -N forcing
-#$ -t 1-100
+#$ -t 1-10
 
-DATE=2019
+DATE=2021
 FOCCUS_DIR=/lustre/storeB/project/fou/hi/foccus/
 OUTDIR=$FOCCUS_DIR/datasets/norkystv3_forcing_zarr
 ZARRFILE=$OUTDIR/forcing_norkystv3_hindcast_${DATE}.zarr
@@ -15,5 +15,5 @@ ZARRFILE=$OUTDIR/forcing_norkystv3_hindcast_${DATE}.zarr
 conda deactivate
 source $FOCCUS_DIR/python-envs/anemoi-env-16-4-25/bin/activate
 #anemoi-datasets init $YAMLFILE $ZARRFILE --overwrite 
-anemoi-datasets load $ZARRFILE --part $SGE_TASK_ID/100 #8784
+anemoi-datasets load $ZARRFILE --part $SGE_TASK_ID/10 #8784
 #anemoi-datasets finalize $ZARRFILE
--- forcing/interp_sub.sh
+++ forcing/interp_sub.sh
@@ -6,13 +6,13 @@
 #$ -e /lustre/storeB/project/fou/hi/foccus/mateuszm/outputs/
 #$ -N hor_interp
 
-ATM_FILE=wrf_3km_norkyst800_prec_2016_ext.nc
+ATM_FILE=arome_meps_2_5km_2021010100-2022010100_ext_newTair.nc
 #arome_meps_2_5km_2017090700_ext.nc #arome_meps_2_5km_2017090609-2018010100_ext.nc
 
 OUTDIR=/lustre/storeB/project/fou/hi/foccus/datasets/norkyst-v3-hindcast/interp_forcings/
 FILE=/lustre/storeB/project/fou/hi/foccus/datasets/norkyst-v3-hindcast/forcing/atm/$ATM_FILE #arome_meps_2_5km_2017010100-2017090606_ext.nc
-VAR=rain
-#Pair,Uwind,Vwind,Tair,Qair,cloud,rain
+VAR=Qair,cloud,rain
+#Pair,Uwind #,Vwind,Tair #,Qair,cloud,rain
 #'Pair', 'Uwind', 'Vwind', 'Tair', 'Qair', 'cloud', 'rain'
 
 python /lustre/storeB/project/fou/hi/foccus/mateuszm/ocean-ai/forcing/interpolate_forcing.py $FILE $OUTDIR $VAR $VAR
