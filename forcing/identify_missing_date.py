244f82339285e28af793673dfc29ed5a47769b6c
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2025-05-23 11:35:06 +0000

    duplicate dates in one file

--- forcing/identify_missing_date.py
+++ forcing/identify_missing_date.py
@@ -20,9 +20,20 @@ def missing_date(file):
         print(time)
         
     return missing_dates
+
+def duplicate_date(file):
+    ds = xr.open_dataset(file)
+    file_times = list(ds.time.values)
+
+    for time in file_times:
+        occ = file_times.count(time)
+        if occ > 1:
+            print(time)
     
 
 if __name__ == '__main__':
     import sys
     file = sys.argv[1]
     missing_date(file)
+    print('******')
+    duplicate_date(file)
