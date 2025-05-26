871c6a5f54732ebc93182e5515fc65a1d1620bf7
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2025-05-26 08:42:50 +0000

    running 10 groups because it's easier to debug / see exactly what failed

--- datasets/incremetal/hindcast-zarr.sh
+++ datasets/incremetal/hindcast-zarr.sh
@@ -1,11 +1,11 @@
 #$ -S /bin/bash
-#$ -l h_rt=0:20:00
+#$ -l h_rt=1:00:00
 #$ -q bigmem-r8.q
 #$ -l h_rss=35G,mem_free=35G,h_data=35G
 #$ -o /lustre/storeB/project/fou/hi/foccus/mateuszm/outputs/
 #$ -e /lustre/storeB/project/fou/hi/foccus/mateuszm/outputs/
 #$ -N surfacehindcast2012
-#$ -t 1-365
+#$ -t 1-10
 
 YEAR=2012
 FOCCUS_DIR=/lustre/storeB/project/fou/hi/foccus/
@@ -15,5 +15,5 @@ ZARRFILE=$OUTDIR/norkystv3_hindcast_${YEAR}_surface.zarr
 conda deactivate
 source $FOCCUS_DIR/python-envs/anemoi-env-16-4-25/bin/activate
 #anemoi-datasets init $YAMLFILE $ZARRFILE --overwrite 
-anemoi-datasets load $ZARRFILE --part $SGE_TASK_ID/365 #8784
+anemoi-datasets load $ZARRFILE --part $SGE_TASK_ID/10 #8784
 #anemoi-datasets finalize $ZARRFILE
