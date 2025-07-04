25c2b6b03cff64fcef6520730b8f39fa84e59b22
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2025-07-04 12:35:32 +0000

    .

--- datasets/incremetal/combine-zarr.sh
+++ datasets/incremetal/combine-zarr.sh
@@ -1,11 +1,11 @@
 #$ -S /bin/bash
-#$ -l h_rt=3:00:00
+#$ -l h_rt=6:00:00
 #$ -q bigmem-r8.q
 #$ -l h_rss=80G,mem_free=80G,h_data=80G
 #$ -o /lustre/storeB/project/fou/hi/foccus/mateuszm/outputs/
 #$ -e /lustre/storeB/project/fou/hi/foccus/mateuszm/outputs/
 #$ -N combine-zarr-files
-#$ -t 1-13
+#$ -t 1-10
 
 FOCCUS_DIR=/lustre/storeB/project/fou/hi/foccus/
 OUTDIR=$FOCCUS_DIR/datasets/
@@ -14,5 +14,5 @@ ZARRFILE=$OUTDIR/norkystv3_hindcast_2012-2024_surface.zarr
 conda deactivate
 source $FOCCUS_DIR/python-envs/anemoi-env-16-4-25/bin/activate
 #anemoi-datasets init $YAMLFILE $ZARRFILE --overwrite 
-anemoi-datasets load $ZARRFILE --part $SGE_TASK_ID/13 #8784
+anemoi-datasets load $ZARRFILE --part $SGE_TASK_ID/10 #8784
 #anemoi-datasets finalize $ZARRFILE
