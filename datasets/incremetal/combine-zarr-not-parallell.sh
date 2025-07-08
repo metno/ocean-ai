0b02570c49dcf1a598f4c670c432951901f97b17
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2025-07-08 10:43:53 +0000

    non-parallell combine submit script

---
+++ datasets/incremetal/combine-zarr-not-parallell.sh
@@ -0,0 +1,20 @@
+#$ -S /bin/bash
+#$ -l h_rt=72:00:00
+#$ -q bigmem-r8.q
+#$ -l h_rss=80G,mem_free=80G,h_data=80G
+#$ -o /lustre/storeB/project/fou/hi/foccus/mateuszm/outputs/
+#$ -e /lustre/storeB/project/fou/hi/foccus/mateuszm/outputs/
+#$ -N combine-zarr-files
+
+FOCCUS_DIR=/lustre/storeB/project/fou/hi/foccus/
+OUTDIR=$FOCCUS_DIR/datasets/
+ZARRFILE=$OUTDIR/norkystv3_hindcast_2012-2024_surface-not-parallell.zarr
+
+conda deactivate
+source $FOCCUS_DIR/python-envs/anemoi-env-7-7-25/bin/activate
+
+for i in {1..10}
+do
+    anemoi-datasets load $ZARRFILE --part $i/10
+done
+
