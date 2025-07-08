b0041b52ad3150b049a4cb3f79881c52b325b1f5
Ina Kullmann <ina.k.kullmann@met.no>
2025-07-08 12:37:01 +0000

    stupid ocean predict plots that were never used

--- plot/plot_infer.py
+++ plot/plot_infer.py
@@ -4,8 +4,9 @@ import xarray as xr
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
-dir = '/lustre/storeB/project/fou/hi/foccus/ina/OceanAI/training/outputs/experiment1/'
-file = dir + '7e296243.nc'
+dir = '/lustre/storeB/project/fou/hi/foccus/experiments/lam-nothin-res10-2024-cloud/inference/'
+#file = dir + 'lam48.nc'
+file = 'lam48.nc'
 ds = xr.open_dataset(file)
 
 print(ds.longitude)
@@ -13,6 +14,6 @@ print(ds.longitude)
 fig = plt.figure(figsize=(20,20))
 ax = plt.axes()
 
-c = ax.scatter(ds.longitude,ds.latitude,c=ds.isel(time=0)['temperature_1'],s=5)
+c = ax.scatter(ds.longitude,ds.latitude,c=ds.isel(time=0)['temperature_0'],s=5)
 
 plt.show()
