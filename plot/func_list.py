509aa233f7748e827736b741d397e1511f31b09a
Malenefk <you@example.com>
2025-07-09 15:00:59 +0000

    changes

--- plot/func_list.py
+++ plot/func_list.py
@@ -11,7 +11,7 @@ import cartopy.crs as ccrs
 #ToDo in use of code:
 #To use the function for several timesteps, call on the function as:
 #for time_indx in [time0,time1,time_n]:
-    #speed(file_name, variable, year, time_indx, cbar_title)
+    #speed(file_name, variable, year, datetime, time_indx, cbar_title)
 
 def speed(file_name, variable, year, datetime, time_indx, cbar_title):
     file_name = file_name #the file you wish to use to plot the variables
@@ -41,7 +41,7 @@ def temp(file_name, variable, year, datetime, time_indx, cbar_title):
     file_name = file_name #the file you wish to use to plot the variables
     ds_file = open_dataset(file_name, select = variable) #open dataset and choose variable from dataset
     fig,ax = plt.subplots(figsize = (8,10), subplot_kw={"projection": ccrs.NorthPolarStereo()})
-    im = ax.scatter(ds_file.longitudes, ds_file.latitudes, c = ds_file[time_indx,0,0,:], s=2, vmin = 30, cmap = cmocean.cm.thermal, transform = ccrs.PlateCarree())
+    im = ax.scatter(ds_file.longitudes, ds_file.latitudes, c = ds_file[time_indx,0,0,:], s=2, cmap = cmocean.cm.thermal, transform = ccrs.PlateCarree())
     ax.add_feature(cartopy.feature.LAND, zorder = 1, edgecolor = "black")
     cax = fig.add_axes([ax.get_position().x1+0.025, ax.get_position().y0, 0.025, ax.get_position().height])
     cbar = fig.colorbar(im, ax=ax, cax=cax, extend = "both")
@@ -49,3 +49,14 @@ def temp(file_name, variable, year, datetime, time_indx, cbar_title):
     ax.set_title(f'+ {time_indx}h from {year} - {datetime}')
     plt.show()
 
+def moisture(file_name, variable, year, datetime, time_indx, cbar_title):
+    file_name = file_name #the file you wish to use to plot the variables
+    ds_file = open_dataset(file_name, select = variable) #open dataset and choose variable from dataset
+    fig,ax = plt.subplots(figsize = (8,10), subplot_kw={"projection": ccrs.NorthPolarStereo()})
+    im = ax.scatter(ds_file.longitudes, ds_file.latitudes, c = ds_file[time_indx,0,0,:], s=2, cmap = cmocean.cm.rain, transform = ccrs.PlateCarree())
+    ax.add_feature(cartopy.feature.LAND, zorder = 1, edgecolor = "black")
+    cax = fig.add_axes([ax.get_position().x1+0.025, ax.get_position().y0, 0.025, ax.get_position().height])
+    cbar = fig.colorbar(im, ax=ax, cax=cax, extend = "both")
+    cbar.ax.set_title(cbar_title, fontsize = 14)
+    ax.set_title(f'+ {time_indx}h from {year} - {datetime}')
+    plt.show()
