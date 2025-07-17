9b179706eba0c108759e4f6f445138112329b44c
Ina Kullmann <ina.k.kullmann@met.no>
2025-07-17 07:11:10 +0000

    scripts to scan for nans in all vars (surface)

--- datasets/find_nans.py
+++ datasets/find_nans.py
@@ -6,6 +6,7 @@
 
 import numpy as np
 from anemoi.datasets import open_dataset
+from anemoi.datasets import MissingDateError
 import sys
 
 if len(sys.argv) > 1:
@@ -28,6 +29,7 @@ f.write(f"# Dataset: {file}\n")
 f.write(f"# Year: {year}\n")
 f.write(f"# Shape of dataset: {ds.shape}\n")
 f.write(f"# Variable names: {variable_names}\n")
+f.write(f"# \n")
 
 #----------------------------------------------------------------------------
 # Check for nans in ocean domain
@@ -42,64 +44,48 @@ ind_not_missing = np.setdiff1d(ind_all, ind_missing)
 # sea mask is constant in time, so just need to use a valid time index
 sea_mask = np.bool(ds[int(ind_not_missing[0]),ds.name_to_index['sea_mask'],0,:])
 
-# Loop over variables and time to save memory
-for ivar in range(ds.shape[1]):
-    found_nans = False
-    print('-----\nChecking variable:', variable_names[ivar])
-    for itime in range(ds.shape[0]):
-        if ivar == ds.name_to_index['sea_mask']:
-            # skip sea_mask
-            continue
+#----------------------------------------------------------------------------
+# Loop over variables and istep time indices at the time to save memory
+istep = 100  # step size for time index
+var_dict = ds.name_to_index 
+for var in var_dict:
+    ivar = var_dict[var]
+    if var == 'sea_mask':
+        # skip sea_mask var
+        continue
+    
+    print(f'# Checking variable: {var} (ivar={ivar})\n')
+    
+    for i in range(0, ds.shape[0], istep):
+        print(f"i= {i}, var= {var}, ivar= {ivar}")
 
-        # get one variable
-        var = ds[itime,ivar,:,:]  # Request enough memory!! About 103 GiB for shape (8784, 1, 3153556) and data type float32
-        var_ocean = var[sea_mask] # get a 1D (flat) array (1822664,)
-        
-        if itime % 500 == 0:
-            # print itime every 500
-            print('itime:', itime)
+        try:
+            ds_var = ds[i:i+istep,ivar,0,:]
+            ind_time,ind_grid = np.where(np.isnan(ds_var))
+            print(f"ind_time={ind_time}, ind_grid={ind_grid}") 
+        except MissingDateError as e:
+            print(f"MissingDateError:",e)
+            i_not_missing = np.intersect1d(ind_not_missing, np.arange(i, i+istep))
+            print(f"i_not_missing={i_not_missing}")
+            if i_not_missing.size != 0:
+                ind_time,ind_grid = np.where(np.isnan(ds[i_not_missing[0]:i_not_missing[-1]+1,ivar,0,:]))
+                ind_time += i_not_missing[0]  # adjust index to the current step
+            else:
+                ind_time = np.array([])  
 
-        if np.isnan(var_ocean).any():
-            found_nans = True
-    if found_nans:
-        print('!! Nans in ocean for variable:', variable_names[ivar]) 
-        # You could loop over time to see where there are nans
-
+        if var not in ['Uwind_eastward','Vwind_northward','u_eastward_0','v_northward_0']:
+            # The above variables has values over land. 
+            # For those that dont, we will only write to file when nans found in the sea
+            intersect_nan_sea = np.intersect1d(ind_grid, np.where(sea_mask)[0], return_indices=True)[0]
+            ind_grid = ind_grid[intersect_nan_sea]
+            ind_time = ind_time[intersect_nan_sea]
+            # this ofthen returns empty arrays
+        if ind_time.size > 0 and ind_grid.size > 0:
+            for n in range(len(ind_time)):
+                it=int(ind_time[n])
+                ig=int(ind_grid[n])
+                f.write(f"{var}: ds[ {i+it}, {ivar}, 0, {ig} ] = {ds[i+it,ivar,0,ig]}\n")
+                # print(f"{var}: ds[ {i+it}, {ivar}, 0, {ig} ] = {ds[i+it,ivar,0,ig]}\n")
 
 f. close()
 
-#---------------------------------------------------------------------------
-# About the dataset
-#---------------------------------------------------------------------------
-# $ anemoi-datasets inspect ../datasets/norkystv3_hindcast_2024_surface.zarr
-# 📦 Path          : ../datasets/norkystv3_hindcast_2024_surface.zarr
-# 🔢 Format version: 0.30.0
-# 
-# 📅 Start      : 2024-01-01 00:00
-# 📅 End        : 2024-12-31 23:00
-# ⏰ Frequency  : 1h
-# 🚫 Missing    : 0
-# 🌎 Resolution : None
-# 🌎 Field shape: [1148, 2747]
-# 
-# 📐 Shape      : 8,784 × 11 × 1 × 3,153,556 (1.1 TiB)
-# 💽 Size       : 418.6 GiB (418.6 GiB)
-# 📁 Files      : 8,890
-# 
-#    Index │ Variable                          │       Min │     Max │      Mean │    Stdev
-#    ──────┼───────────────────────────────────┼───────────┼─────────┼───────────┼─────────
-#        0 │ Uwind_eastward                    │    -28.02 │   34.87 │  0.312835 │  6.23567
-#        1 │ Vwind_northward                   │    -29.44 │   28.97 │   1.31028 │  6.19878
-#        2 │ h                                 │        10 │ 3259.61 │    399.75 │  745.999
-#        3 │ salinity_-0.004903846153846154    │ -0.338001 │   36.23 │    34.159 │  2.07608
-#        4 │ sea_mask                          │         0 │       1 │  0.577971 │ 0.493883
-#        5 │ temperature_-0.004903846153846154 │    -3.565 │  21.845 │   8.75577 │  3.65528
-#        6 │ u_eastward_-0.004903846153846154  │    -6.043 │   5.664 │ 0.0200142 │ 0.151678
-#        7 │ ubar_eastward                     │    -5.548 │   5.205 │ 0.0148608 │ 0.126217
-#        8 │ v_northward_-0.004903846153846154 │    -4.319 │   6.173 │ 0.0211188 │ 0.154488
-#        9 │ vbar_northward                    │    -3.991 │   5.792 │ 0.0131414 │ 0.131907
-#       10 │ zeta                              │     -3.85 │    2.41 │  -0.38284 │ 0.472634
-#    ──────┴───────────────────────────────────┴───────────┴─────────┴───────────┴─────────
-# 🔋 Dataset ready, last update the 2nd of this month.
-# 📊 Statistics ready.
-#---------------------------------------------------------------------------
--- datasets/ppi-sub-find-nans.sh
+++ datasets/ppi-sub-find-nans.sh
@@ -1,20 +1,18 @@
 #$ -S /bin/bash
-#$ -l h_rt=01:00:00
+#$ -l h_rt=03:00:00
 #$ -q research-r8.q
-#$ -l h_rss=16G,mem_free=16G,h_data=16G
+#$ -l h_rss=64G,mem_free=64G,h_data=64G
 #$ -o /lustre/storeB/project/fou/hi/foccus/outputs/$JOB_NAME_$JOB_ID_$TASK_ID.out
 #$ -e /lustre/storeB/project/fou/hi/foccus/outputs/$JOB_NAME_$JOB_ID_$TASK_ID.err
 #$ -N find_nans
-#$ -cwd
-#$ -t 1-11
+#$ -t 1-13
 
-YEAR_LIST=(2013 2014 2015 2016 2017 2018 2019 2020 2021 2022 2023 2024)
-# TODO: test 2012 as well
+YEAR_LIST=(2012 2013 2014 2015 2016 2017 2018 2019 2020 2021 2022 2023 2024)
 
 YEAR=${YEAR_LIST[$SGE_TASK_ID-1]}
 FOCCUS_DIR=/lustre/storeB/project/fou/hi/foccus/
 
-echo "Running find_nans for year $YEAR"
+#echo "Running find_nans for year $YEAR"
 conda deactivate
-source $FOCCUS_DIR/.venv/bin/activate
-python3 find_nans.py $YEAR
+source $FOCCUS_DIR/ina/ocean-ai/.venv/bin/activate
+python3 $FOCCUS_DIR/ina/ocean-ai/datasets/find_nans.py $YEAR
