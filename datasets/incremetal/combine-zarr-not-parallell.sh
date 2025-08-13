a5c033ca67a3f418e2201166dfa1c7827edbdbf4
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2025-08-13 12:13:47 +0000

    a little more general

--- datasets/incremetal/combine-zarr-not-parallell.sh
+++ datasets/incremetal/combine-zarr-not-parallell.sh
@@ -2,21 +2,25 @@
 #$ -l h_rt=72:00:00
 #$ -q bigmem-r8.q
 #$ -l h_rss=80G,mem_free=80G,h_data=80G
-#$ -o /lustre/storeB/project/fou/hi/foccus/mateuszm/outputs/
-#$ -e /lustre/storeB/project/fou/hi/foccus/mateuszm/outputs/
-#$ -N combine-zarr-files
+#$ -o /lustre/storeB/project/fou/hi/foccus/outputs/
+#$ -e /lustre/storeB/project/fou/hi/foccus/outputs/
+#$ -N anemoi-datasets
 
 FOCCUS_DIR=/lustre/storeB/project/fou/hi/foccus/
 OUTDIR=$FOCCUS_DIR/datasets/
-ZARRFILE=$OUTDIR/norkystv3_hindcast_2012-2024_surface-not-parallell.zarr
+ZARRFILE=$OUTDIR/ <--- PUT IN .ZARR FILE --->
 
 conda deactivate
 source $FOCCUS_DIR/python-envs/anemoi-env-7-7-25/bin/activate
-#anemoi-datasets init $YAMLFILE $ZARRFILE --overwrite 
-anemoi-datasets load $ZARRFILE --part $SGE_TASK_ID/10 #8784
-#anemoi-datasets finalize $ZARRFILE
+
+# No more parallell, because there are some buggs where it doesn't finish. 
+# This is more consistent. 
+
+
+#anemoi-datasets init $YAMLFILE $ZARRFILE --overwrite
 for i in {1..10}
 do
     anemoi-datasets load $ZARRFILE --part $i/10
 done
 
+#anemoi-datasets finalize $ZARRFILE
