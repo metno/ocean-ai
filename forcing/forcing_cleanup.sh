6d52beff06602f9527269742177ab497e764a968
Ina Kullmann <inakbk@protonmail.com>
2025-12-13 10:00:41 +0100

    Update job runtime limit in forcing_cleanup.sh
    Reduced runtime limit for the job from 10 hours to 4 hours.

--- forcing/forcing_cleanup.sh
+++ forcing/forcing_cleanup.sh
@@ -1,5 +1,5 @@
 #$ -S /bin/bash
-#$ -l h_rt=10:00:00
+#$ -l h_rt=04:00:00
 #$ -q research-r8.q
 #$ -l h_rss=8G,mem_free=8G,h_data=8G
 #$ -o /lustre/storeB/project/fou/hi/foccus/outputs/$JOB_NAME_$JOB_ID.out
@@ -11,9 +11,11 @@
 # forcing_cleanup.py is called
 
 # Use this script to remove uneccecary data from the atm forcing files (and save space)
+echo "Cleanup atm forcing files to save space"
 source /modules/rhel8/mamba-mf3/etc/profile.d/conda.sh
 conda activate 2025-01-development
 python3 /lustre/storeB/project/fou/hi/foccus/forcing_cleanup.py
+
 
 # Now we may concat the files with a command similar to this:
 #ncrcat -O -d time,0,366 $DIR/norkyst_clm_${YEAR}${MONTH}${DAY}T00Zm00.nc $DIR/norkyst_clm_${YEAR}m00.nc
