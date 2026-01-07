96bffde9f4c52915250947c574e9140efb79f928
Malenefk <you@example.com>
2026-01-07 16:53:23 +0000

    change mem allowance and try a new run

--- plot/seasonal_avg.sh
+++ plot/seasonal_avg.sh
@@ -1,7 +1,7 @@
 #$ -S /bin/bash
 #$ -l h_rt=01:00:00
 #$ -q research-r8.q
-#$ -l h_rss=30G,mem_free=30G,h_data=30G
+#$ -l h_rss=50G,mem_free=50G,h_data=50G
 #$ -o /lustre/storeB/project/fou/hi/foccus/outputs/$JOB_NAME_$JOB_ID.out
 #$ -e /lustre/storeB/project/fou/hi/foccus/outputs/$JOB_NAME_$JOB_ID.err
 #$ -N seasonal_avg
