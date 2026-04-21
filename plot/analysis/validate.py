627e06c38d0d75237053721f351e57ffbb308c6e
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2026-04-21 12:42:38 +0000

    initial validate script

---
+++ plot/analysis/validate.py
@@ -0,0 +1,16 @@
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
+
+#files = '/lustre/storeB/project/fou/hi/foccus/mateuszm/results/may2024/*'
+
+#ds = xr.open_mfdataset(files)
+
+ds = xr.open_dataset('/lustre/storeB/project/fou/hi/foccus/mateuszm/results/may2024/2024-05-01_24h_18d28_e011_s050000.nc')
+
+def spectra():
+    pass
+
+
+if __name__ == '__main__':
+    spectra()
